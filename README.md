4cbaf08502f1865b1789e4765fbedfc96c2dd4c2
Enderson Marín <46828206+ENDERSON-MARIN@users.noreply.github.com>
2022-04-13 23:52:10 -0500

    Component Banner Ready!

--- README.md
+++ README.md
@@ -32,3 +32,22 @@
 ## CREAR LOS ESTILOS PARA LOS COMPONENTES DE PAGES
 
 -Patrón para anadir la infraestructura de estilos a todos los componentes de páginas: Banner, Header, Plans, Rows
+
+## COMPONENTE HEADER
+
+-Importamos el logo
+-Añadimos al AppBar una clase transparent condicional, esta clase se activa si la variable show = true.
+-La variable show se concierte en true cuando hacemos un scroll vertical mayor a 100px.
+-Para escuchar cuando el usuario hace scroll vertical añadimos un event listener al window.
+-El eventListener está activo una sola vez cada vez que refrescamos la página mediante el hook (useEffect con []).
+-Una vez montado el componente hay que limpiarlo para que no nos quede colgado el listener.
+-Añadimos las rutas tanto al logo como al Avatar.
+
+## COMPONENTE BANNER
+
+-Hemos importado una imagen para el backgroundImage
+-En React tenemos que acompañar las bacgroundsImages con estilos como object-fit, background-size y background-position.
+-Hemos posicionado el título de la pelicula, los botones y la descripción.
+-Como la descripción va a venir de la API tenemos que asegurarnos de truncarla para que quepa, para ello hemos declarado la función truncate.
+-Anadimos un efecto fade al final de la imagen mediante u div vacio con estilos.
+
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,23 +1,72 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core";
-import heroBanner from "../images/casa_de_papel.jpeg"
+import { Button, makeStyles, Typography } from "@material-ui/core";
+import heroBanner from "../images/casa_de_papel.jpeg";
 
 const Banner = () => {
   const classes = useStyles();
+
+  const truncate = (string, n) =>
+    string?.length > n ? `${string.substring(0, n - 1)} ...` : string;
   return (
     <div className={classes.root}>
+      <div className={classes.content}>
+        <Typography variant="h2" component="h1">
+          Movie Title
+        </Typography>
+        <div className={classes.buttons}>
+          <Button>Play</Button>
+          <Button>My List</Button>
+        </div>
+        <Typography
+          style={{ wordWrap: "break-word" }}
+          variant="h6"
+          className={classes.description}
+        >
+          {truncate(
+            "Movie Description Movie Description Movie Description Movie Description Movie Description Movie Description Movie Description Movie Description Movie Description Movie Description Movie Description Movie Description Movie Description Movie Description Movie Description Movie Description Movie Description Movie Description Movie Description Movie Description Movie Description Movie Description Movie Description Movie Description Movie Description Movie Description Movie Description Movie Description Movie Description Movie Description Movie Description Movie Description Movie Description Movie Description Movie Description Movie Description Movie Description Movie Description ",
+            160
+          )}
+        </Typography>
+        <div className={classes.fadeEfect}></div>
+      </div>
     </div>
   );
 };
 
 const useStyles = makeStyles((theme) => ({
   root: {
-      backgroundImage:`url(${heroBanner})`,
-      position: "relative",
-      height: "500px",
-      objectFit:"contain",
-      backgroundSize:"cover",
-      backgroundPosition:"center"
+    backgroundImage: `url(${heroBanner})`,
+    position: "relative",
+    height: "500px",
+    objectFit: "contain",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    color: "#fff",
+  },
+  buttons: {
+    "& button": {
+      cursor: "pointer",
+      color: "#fff",
+      fontWeight: 700,
+      borderRadius: "5px",
+      padding: theme.spacing(1, 4, 1, 4),
+      marginRight: "1rem",
+      backgroundColor: "rgba(51,51,51,0.5)",
+    },
+    "& button:hover": {
+      color: "#000",
+      backgroundColor: "#e6e6e6",
+    },
+  },
+  fadeEfect: {
+    position: "absolute",
+    top: "30vh",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    zIndex: 99,
+    backgroundImage:
+      "linear-gradient(180deg, transparent, rgba(37,37,37,0.61), #111)",
   },
 }));
 
